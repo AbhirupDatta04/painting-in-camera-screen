6a0d42fb062e149ffea96639c5cbf254c54a4ab1
Abhirup Datta <66831444+AbhirupDatta04@users.noreply.github.com>
2021-07-27 20:49:14 +0530

    Final commit

--- Painting.py
+++ Painting.py
@@ -65,3 +65,73 @@ while True:
     cnts = cnts[0] if len(cnts) == 2 else cnts[1]
     center = None
     
+    if len(cnts) > 0:
+        cnt = sorted(cnts, key = cv2.contourArea, reverse = True)[0]
+        # Radius of the enclosing circle around contour
+        ((x, y), radius) = cv2.minEnclosingCircle(cnt)
+        cv2.circle(frame, (int(x), int(y)), int(radius), (0, 255, 255), 2)
+        # moments to calculate the center of the contour (in this case Circle)
+        M = cv2.moments(cnt)
+        center = (int(M['m10'] / M['m00']), int(M['m01'] / M['m00']))
+
+        if center[1] <= 65:
+            if 40 <= center[0] <= 140: # Clear All
+                bpoints = [deque(maxlen=512)]
+                gpoints = [deque(maxlen=512)]
+                rpoints = [deque(maxlen=512)]
+                ypoints = [deque(maxlen=512)]
+
+                bindex = 0
+                gindex = 0
+                rindex = 0
+                yindex = 0
+
+                paintWindow[67:,:,:] = 255
+            elif 160 <= center[0] <= 255:
+                    colorIndex = 0 # Blue
+            elif 275 <= center[0] <= 370:
+                    colorIndex = 1 # Green
+            elif 390 <= center[0] <= 485:
+                    colorIndex = 2 # Red
+            elif 505 <= center[0] <= 600:
+                    colorIndex = 3 # Yellow
+        else :
+            if colorIndex == 0:
+                bpoints[bindex].appendleft(center)
+            elif colorIndex == 1:
+                gpoints[gindex].appendleft(center)
+            elif colorIndex == 2:
+                rpoints[rindex].appendleft(center)
+            elif colorIndex == 3:
+                ypoints[yindex].appendleft(center)
+  
+    else:
+        bpoints.append(deque(maxlen=512))
+        bindex += 1
+        gpoints.append(deque(maxlen=512))
+        gindex += 1
+        rpoints.append(deque(maxlen=512))
+        rindex += 1
+        ypoints.append(deque(maxlen=512))
+        yindex += 1
+
+    # lines of colors (Blue, Green, Red and Yellow)
+    points = [bpoints, gpoints, rpoints, ypoints]
+    for i in range(len(points)):
+        for j in range(len(points[i])):
+            for k in range(1, len(points[i][j])):
+                if points[i][j][k - 1] is None or points[i][j][k] is None:
+                    continue
+                cv2.line(frame, points[i][j][k - 1], points[i][j][k], colors[i], 2)
+                cv2.line(paintWindow, points[i][j][k - 1], points[i][j][k], colors[i], 2)
+
+    # Show the frame and the paintWindow image
+    cv2.imshow("Tracking", frame)
+    cv2.imshow("Paint", paintWindow)
+
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
+
+cam.release()
+cv2.destroyAllWindows()
+    
